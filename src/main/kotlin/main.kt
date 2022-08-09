7b4e1774900ab4c59e8addb6a6353397cd81cceb
hmaeta <hmaeta21+git@gmail.com>
2022-08-09 19:07:12 +0900

    loopを定義

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,16 @@
+fun loop(maxIterations: Int) {
+    //  state = memory & registers
+
+    repeat(maxIterations) {
+        //  CPUがstateを更新
+
+        //  PPUがstate.memory.VRAM領域を見て画面を更新
+        //  APUがstate.memory.AUDIO領域を見て音を出す
+        //  割り込みがあったらコールバックを実行
+    }
+}
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    loop(10)
+    println("Emulation finished")
 }
