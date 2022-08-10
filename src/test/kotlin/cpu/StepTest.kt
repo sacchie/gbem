cf2fba6ed98d810ea78e698d62b1bfe0814601c0
hmaeta <hmaeta21+git@gmail.com>
2022-08-10 09:59:19 +0900

    テストのリファクタリング

--- src/test/kotlin/cpu/StepTest.kt
+++ src/test/kotlin/cpu/StepTest.kt
@@ -4,26 +4,23 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class StepTest {
+
+    private fun makeMemory(vals: List<Int8>) = object : Memory {
+        override fun get8(addr: Int16): Int8 = vals[addr.toInt()]
+    }
+
     @Test
     fun `op_LD`() {
         val OPCODE: Int8 = 0b01_001_011
 
-        val values = RegisterValues(
+        val regValues = RegisterValues(
             pc = 0,
             reg8 = mutableMapOf(Reg8.E to 0x02)
         )
-        val memory = object : Memory {
-            override fun get8(addr: Int16): Int8 {
-                if (addr == 0.toShort()) {
-                    return OPCODE
-                }
-                throw RuntimeException()
-            }
-        }
 
-        step(memory, Registers(values))
+        step(makeMemory(listOf(OPCODE)), Registers(regValues))
 
-        assertEquals(0x02, values.reg8[Reg8.C]!!, )
-        assertEquals(1, values.pc)
+        assertEquals(0x02, regValues.reg8[Reg8.C]!!)
+        assertEquals(1, regValues.pc)
     }
 }
