8999739ddd09b4f490677b9c99882ab965748372
Kaiichiro <kigh.ota@gmail.com>
2022-08-15 21:37:14 +0900

    WIP

---
+++ src/main/kotlin/ppu/ppu.kt
@@ -0,0 +1,12 @@
+package ppu
+
+// 160x144 pixel LCD
+// 8x8 tiles (20x18)
+// palette
+// 3 layers: background, window, objects
+
+// VRAM: $8000-97FF (0x1800(6144) bytes, 16 bytes/tile, 384 tiles)
+
+fun render() {
+
+}
