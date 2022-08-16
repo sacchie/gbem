92aa1d31e93ef9e7dd23bfaf67d00d4cf852e878
Kaiichiro Ota <kaiichiro-ota@cybozu.co.jp>
2022-08-17 02:56:51 +0900

    WIP 2022-08-16

--- src/main/kotlin/ppu/ppu.kt
+++ src/main/kotlin/ppu/ppu.kt
@@ -6,7 +6,33 @@ package ppu
 // 3 layers: background, window, objects
 
 // VRAM: $8000-97FF (0x1800(6144) bytes, 16 bytes/tile, 384 tiles)
+// Tile maps: $9800-$9BFF, $9C00-$9FFF
 
+// VRAM state, some I/O registers -> screen pixels
 fun render() {
+    // read VRAM
 
+    // get Block0, OBJ0 tile data
+    vram.
+
+    // reconstruct pixel states
+}
+
+typealias Address = Int
+typealias Int8 = Int
+typealias Int2 = Int
+
+interface VRAM {
+    fun get(addr: Address): Int8
+
+    fun getPixelAt(x: Int, y: Int, startAddress: Address): Int2
+}
+
+class VRAMImpl : VRAM {
+    override fun get(addr: Address): Int8 {
+        TODO("Not yet implemented")
+    }
+    override fun getPixelAt(x: Int, y: Int, startAddress: Address): Int2 {
+        TODO("Not yet implemented")
+    }
 }
