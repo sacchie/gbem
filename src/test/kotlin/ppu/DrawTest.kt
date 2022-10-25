3452dc90556b248d899c350984ec38acb3c39542
Kaiichiro Ota <kaiichiro-ota@cybozu.co.jp>
2022-10-25 18:34:31 +0900

    add unit test

--- src/test/kotlin/ppu/DrawTest.kt
+++ src/test/kotlin/ppu/DrawTest.kt
@@ -1,15 +1,44 @@
 package ppu
 
-import cpu.CommandLdR8R8
-import cpu.RegEnum8
-import cpu.Registers
-import cpu.run
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class DrawTest {
     @Test
-    fun drawBackgroundTileToScreen() {
-        // TODO
+    fun testDrawBackgroundTileToScreen() {
+        val set = mutableSetOf<Triple<Int, Int, Int2>>()
+        val dummyData =
+            listOf(0x3C, 0x7E, 0x42, 0x42, 0x42, 0x42, 0x42, 0x42, 0x7E, 0x5E, 0x7E, 0x0A, 0x7C, 0x56, 0x38, 0x7C)
+        drawBackgroundTileToScreen(object : Memory {
+            override fun get(addr: Address): Int8 {
+                return when (addr) {
+                    in 0x8010..0x801F -> dummyData[addr % 16]
+                    else -> error("unexpected")
+                }
+            }
+        }, 5, 5, 1, 0x8000) { x, y, colorId ->
+            set.add(
+                Triple(
+                    x,
+                    y,
+                    colorId
+                )
+            )
+        }
+        assertThat(set).hasSize(64)
+        listOf(
+            // @formatter:off
+            0b00, 0b10, 0b11, 0b11, 0b11, 0b11, 0b10, 0b00,
+            0b00, 0b11, 0b00, 0b00, 0b00, 0b00, 0b11, 0b00,
+            0b00, 0b11, 0b00, 0b00, 0b00, 0b00, 0b11, 0b00,
+            0b00, 0b11, 0b00, 0b00, 0b00, 0b00, 0b11, 0b00,
+            0b00, 0b11, 0b01, 0b11, 0b11, 0b11, 0b11, 0b00,
+            0b00, 0b01, 0b01, 0b01, 0b11, 0b01, 0b11, 0b00,
+            0b00, 0b11, 0b01, 0b11, 0b01, 0b11, 0b10, 0b00,
+            0b00, 0b10, 0b11, 0b11, 0b11, 0b10, 0b00, 0b00,
+            // @formatter:on
+        ).forEachIndexed { i, b ->
+            assertThat(set).contains(Triple(40 + i % 8, 40 + i / 8, b))
+        }
     }
 }
